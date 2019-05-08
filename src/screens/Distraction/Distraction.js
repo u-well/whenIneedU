9cf3c387403e81be24bc92548070f370d4ebf65b
Neota Moe <neota.moe@gmail.com>
2019-05-07 20:48:17 -0500

    adding video screen and hard-coded video

--- src/screens/Distraction/Distraction.js
+++ src/screens/Distraction/Distraction.js
@@ -1,9 +1,28 @@
 import React, {Component} from 'react';
 import {SectionList, View, Text, StyleSheet} from 'react-native';
 import ListItem from '../../components/ListItem/ListItem';
+import {Navigation} from 'react-native-navigation';
 
 class DistractionScreen extends Component {
     state = {
+    }
+
+    goToVideo = () => {
+        Navigation.push(this.props.componentId, {
+            component: {
+              name: 'whenIneedU.Video',
+              passProps: {
+                text: 'Video'
+              },
+              options: {
+                topBar: {
+                  title: {
+                    text: 'Distraction Video'
+                  }
+                }
+              }
+            }
+        });
     }
 
     render () {
@@ -15,7 +34,7 @@ class DistractionScreen extends Component {
                 {title: 'Activities', data: ['Activity 1', 'Activity 2', 'Activity 3', 'Activity 4', 'Activity 5',]},
                 {title: 'Videos', data: ['Video A', 'Video B', 'Video C']},
               ]}
-              renderItem={({item}) => <ListItem style={styles.item} image={require('../../assets/images/chicken.png')} itemName={item}></ListItem>}
+              renderItem={({item}) => <ListItem style={styles.item} image={require('../../assets/images/chicken.png')} itemName={item} onItemPressed={this.goToVideo}></ListItem>}
               renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
               keyExtractor={(item, index) => index}
             />
